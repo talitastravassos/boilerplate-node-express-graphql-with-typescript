8c6616af9f0daf41e18fbb514aa334f4e5d473fd
talita.travassos <talita.travassos.external@lojasrenner.com.br>
2023-09-15 14:43:13 -0300

    add first commit

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import '@controllers/DummyController';
+import * as dotenv from 'dotenv';
+import express from 'express';
+
+const app = express();
+
+dotenv.config();
+
+const port = 3333;
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.listen(port, () => {
+  console.log(`⚡ Server running on port ${port} ⚡`);
+});
